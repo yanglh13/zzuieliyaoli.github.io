efd40cf73064ca9880908041d035a50e60688edf
zzuieliyaoli <zzuieliyaoli@gmail.com>
2014-12-14 18:41:02 +0800

    back-top

--- scripts/lee.js
+++ scripts/lee.js
@@ -1,20 +1,25 @@
 window.onload = function(){
-	var elem = document.querySelector(".back-top");
+	
 
+	var elem = document.querySelector(".back-top");
+	
+	if (document.body.scrollTop >= 100){
+				elem.style.display = "inline";
+	}
+	
 	elem.addEventListener("click", function(event){
 		event.preventDefault();
 		setTimeout(function(){
-			document.body.scrollTop = document.body.scrollTop - 200;
+			document.body.scrollTop = document.body.scrollTop - (document.body.scrollTop / 3);
 
 			if (document.body.scrollTop > 0) {
-				setTimeout(arguments.callee, 50);
+				setTimeout(arguments.callee, 30);
 			}
 
-		}, 50);
+		}, 30);
 	}, false);
 
 	document.addEventListener("scroll", function(){
-
 		clearTimeout(tId);
 		var tId = setTimeout(function(){
 			if (document.body.scrollTop < 100) {
@@ -30,48 +35,3 @@ window.onload = function(){
 
 
 };
-	// elem.addEventListener("click", function(event){
-	// 	event.preventDefault();
-	    
-	// 	var tId = setInterval(function(){
-	// 		if (document.body.scrollTop < 0) {
-	// 			clearInterval(tId);
-	// 			console.log("stop");
-	// 		}
-
-	// 		document.body.scrollTop = document.body.scrollTop - 100;
-			
-	// 	},50);		
-	// },false );
-
-	
-	// function throttle(method, context){
-	//   clearTimeout(method.tId);
-
-	//   method.tId = setTimeout(function(){
-	//     method.call(context);
-	//   }, 100);
-	// }	
-
-	// elem.addEventListener("click", scrollTop, false);
-
-	// function scrollTop(event){
-	// 	event.preventDefault();
-	//     var scroll = document.body.scrollTop || document.documentElement.scrollTop;
-	// 	throttle(function(){
-	// 		setTimeout(function(){
-	// 			scroll = scroll -10;
-	// 		},100);
-			
-	// 	});			
-	// }
-
-	
-
-
-
-// 事件对象、运动框架
-// setTimeout setInterval
-// var e = document.body.scrollTop;
-
-// 注意区别【持续发生】和【间歇调用】
