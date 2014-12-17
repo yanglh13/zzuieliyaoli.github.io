e89e54d97524e4450ed6d6034a82cac3f75f0028
zzuieliyaoli <zzuieliyaoli@gmail.com>
2014-12-17 22:42:33 +0800

    post

--- scripts/lee.js
+++ scripts/lee.js
@@ -2,17 +2,19 @@ window.onload = function(){
 	
 
 	var elem = document.querySelector(".back-top");
+
 	
-	if (document.body.scrollTop >= 100){
+	if ((document.documentElement.scrollTop + document.body.scrollTop) >= 100){
 				elem.style.display = "inline";
 	}
 	
 	elem.addEventListener("click", function(event){
 		event.preventDefault();
 		setTimeout(function(){
+			document.documentElement.scrollTop = document.documentElement.scrollTop - (document.documentElement.scrollTop / 3);
 			document.body.scrollTop = document.body.scrollTop - (document.body.scrollTop / 3);
 
-			if (document.body.scrollTop > 0) {
+			if ((document.documentElement.scrollTop + document.body.scrollTop) > 0) {
 				setTimeout(arguments.callee, 30);
 			}
 
@@ -22,10 +24,10 @@ window.onload = function(){
 	document.addEventListener("scroll", function(){
 		clearTimeout(tId);
 		var tId = setTimeout(function(){
-			if (document.body.scrollTop < 100) {
+			if ((document.documentElement.scrollTop + document.body.scrollTop) < 100) {
 				elem.style.display = "none";
 			}
-			if (document.body.scrollTop >= 100){
+			if ((document.documentElement.scrollTop + document.body.scrollTop) >= 100){
 				elem.style.display = "inline";
 			}
 
@@ -35,3 +37,5 @@ window.onload = function(){
 
 
 };
+
+
